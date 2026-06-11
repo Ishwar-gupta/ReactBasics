0357a6de6b5a30e418407b1b6a0fe3ce70b31c92
Siddheshwar Gupta <siddheshwargupta081@gmail.com>
2026-06-11 17:58:34 +0545

    Add React projects (01vitereact + my-app)

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Learning React with chai aur code | Siddheshwar Gupta</h1>;
+}
+
+export default App;
---
+++ my-app/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>learning React.js</h1>;
+}
+
+export default App;
