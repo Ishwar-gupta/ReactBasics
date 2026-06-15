d178006e829c50a2414baa4ff6113fa9c781feee
Siddheshwar Gupta <siddheshwargupta081@gmail.com>
2026-06-15 18:12:37 +0545

    first commit

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,11 +2,13 @@ import Chai from "./Chai";
 import Navbar from "./Navbar";
 
 function App() {
+  const username = "Learning by chai aur code youtube channel";
   return (
     <>
       <Navbar />
       <Chai />
-      <h1>Learning React with chai aur code | Siddheshwar Gupta</h1>;
+      <h1>Learning React with chai aur code | Siddheshwar Gupta {username}</h1>;
+      //{username} is evaluated expression
       <p>but she is too innocent and pretty</p>;
     </>
   );
